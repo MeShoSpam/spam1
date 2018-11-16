633b1d7556dadd6017cde73f52cc8fd3aa9fc23b
MeShoSpam <45091970+MeShoSpam@users.noreply.github.com>
2018-11-16 00:29:47 -0800

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("509367668944404480")
+setInterval(function() {
+channel.send(`google`);
+}, 25)
+})
+ 
+ 
+client.login('NTA4MDAyMTYzNDU3MzkyNjYw.DsIj7A.7aUX5ZI2BRz30RdoV7yG2A00yc4');
